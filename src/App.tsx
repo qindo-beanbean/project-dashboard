06bc3af89cbcd917a49f9c00ea0f6a82e8360de1
杜沁 <duqin@duqins-MBP.lan>
2023-11-02 01:05:38 +0800

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BellOutlined } from '@ant-design/icons';
+import { Badge } from 'antd';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello! 
-      This is Qindo's Project DashBoard!
+    <div className='page-container'>
+      <header className='header'>
+        <div className='user-info'>
+          <Badge dot
+            className='bell-icon'
+            size="default"
+            offset={[-4,4]}
+          >
+            <BellOutlined style={{ fontSize: 16 }} />
+          </Badge>
+          <div className='user-name'>Sarah Green</div>
+          <div className='user-avatar'>
+            <img src='src/common/avatar.svg'></img>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
