1ffd5b6549cd4286f41022509e97b1f1c18a1662
杜沁 <duqin@duqins-MBP.lan>
2023-11-02 00:24:20 +0800

    create-react-app by typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello! 
+      This is Qindo's Project DashBoard!
+    </div>
+  );
+}
+
+export default App;
